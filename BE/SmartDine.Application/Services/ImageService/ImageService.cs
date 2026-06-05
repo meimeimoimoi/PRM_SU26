588da43e908d506f67407baacb345c062f65f8ef
thinh <thinhtnhse184470@fpt.edu.vn>
2026-06-05 13:19:17 +0700

    feat: implement ImageService with Supabase storage integration in new Application project

---
+++ BE/SmartDine.Application/Services/ImageService/ImageService.cs
@@ -0,0 +1,53 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Supabase;
+using SupabaseClient = Supabase.Client;
+namespace SmartDine.Application.Services.ImageService
+{
+    public class ImageService
+    {
+        private readonly IConfiguration _configuration;
+
+
+        public ImageService(
+            IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public async Task<string> UploadImageAsync(
+      Stream imageStream,
+      string fileName)
+        {
+            var url = _configuration["Supabase:Url"];
+            var apiKey = _configuration["Supabase:ApiKey"];
+            var bucketName = _configuration["Supabase:BucketName"];
+
+            var client = new Client(
+                url,
+                apiKey,
+                new SupabaseOptions());
+
+            await client.InitializeAsync();
+
+            var path = $"{Guid.NewGuid()}-{fileName}";
+
+            byte[] fileBytes;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await imageStream.CopyToAsync(memoryStream);
+                fileBytes = memoryStream.ToArray();
+            }
+
+            await client.Storage
+                .From(bucketName)
+                .Upload(fileBytes, path);
+
+            return client.Storage
+                .From(bucketName)
+                .GetPublicUrl(path);
+        }
+    }
+}
